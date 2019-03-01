a88004e2e958eecb344f1883bce5c068717f5037
Adam Soffer <adam@soffer.space>
2019-02-28 23:54:55 -0500

    Format masthead file

--- components/Masthead/index.tsx
+++ components/Masthead/index.tsx
@@ -1,11 +1,7 @@
-import {Button} from '@material-ui/core'
-import { Container } from '../../lib/helpers'
-import {
-  Root,
-  Subheading,
-  Heading
-} from './styles'
-import * as Scroll from 'react-scroll';
+import { Button } from "@material-ui/core";
+import { Container } from "../../lib/helpers";
+import { Root, Subheading, Heading } from "./styles";
+import * as Scroll from "react-scroll";
 
 let ScrollLink = Scroll.Link;
 
@@ -14,9 +10,15 @@ export default () => {
     <Root>
       <Container>
         <Heading>✧･ﾟ Livepeer Studio ･ﾟ✧</Heading>
-        <Subheading>Building products & tools for the Livepeer ecosystem.</Subheading>
-        <Button variant="contained" color="primary"><ScrollLink to="products" smooth={true}>View Products & Tools</ScrollLink></Button>
+        <Subheading>
+          Building products & tools for the Livepeer ecosystem.
+        </Subheading>
+        <Button variant="contained" color="primary">
+          <ScrollLink to="products" smooth={true}>
+            View Products & Tools
+          </ScrollLink>
+        </Button>
       </Container>
     </Root>
-  )
-}
+  );
+};
