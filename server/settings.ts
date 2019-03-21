36e70a05b773b7fa1f4f313fabaed9b35fe39f47
Adam Soffer <adam@soffer.space>
2019-03-21 08:26:42 -0400

    Update graph node api

--- server/settings.ts
+++ server/settings.ts
@@ -1,19 +1,18 @@
-let dev = process.env.NODE_ENV !== "production";
+let dev = process.env.NODE_ENV !== 'production'
 
 const settings = {
-  url: dev ? "https://livepeer-studio.serveo.net" : "https://livepeer.studio",
-  graphAPI:
-    "https://api.thegraph.com/subgraphs/name/adamsoffer/livepeer-canary",
+  url: dev ? 'https://livepeer-studio.serveo.net' : 'https://livepeer.studio',
+  graphAPI: 'http://35.231.3.157:8000/subgraphs/name/livepeer/graphql',
   alertsTemplateID: dev
-    ? "d-a8b3a54c545a46e2ba7205dbab07ce8a"
-    : "d-87642cf59bb0447a860d6b7fdd79f768",
+    ? 'd-a8b3a54c545a46e2ba7205dbab07ce8a'
+    : 'd-87642cf59bb0447a860d6b7fdd79f768',
   unbondedTemplateID: dev
-    ? "d-c703e5ba590a46a4b078ce491fe35369"
-    : "d-5b8d14de939147a08a6579b4c0e42051",
+    ? 'd-c703e5ba590a46a4b078ce491fe35369'
+    : 'd-5b8d14de939147a08a6579b4c0e42051',
   confirmationTemplateID: dev
-    ? "d-bf0cb17ed98a46048c0dc6486a02d9e1"
-    : "d-b3812189ecf74b92aefe1d98b34ec054",
-  paused: false
-};
+    ? 'd-bf0cb17ed98a46048c0dc6486a02d9e1'
+    : 'd-b3812189ecf74b92aefe1d98b34ec054',
+  paused: false,
+}
 
-export default settings;
+export default settings
